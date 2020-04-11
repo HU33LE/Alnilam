ddf1ba0ecfcaec249225a3fbac66287f660eba09
Manuel-DaSilva <manueldasilva98@gmail.com>
2020-04-11 15:29:32 -0500

    Validator validating nothing

--- App.js
+++ App.js
@@ -1,23 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const usersController = require('./Controllers/UsersController');
-const { check } = require('express-validator');
 
 const App = express();
 
 const __PORT = 3000;
 
 // --------------- DECLARATION OF MIDDLEWARE -------------
-App.use(express.json());
+// App.use(express.json());
 App.use(bodyParser.json());
 App.use(bodyParser.urlencoded({extended: false}));
 
-App.post('/users/new',[
-    check('firstName').isEmpty(),
-    check('lastName').isEmpty,
-    check('email').isEmail().isEmpty(),
-    check('password').isLength({ min: 6 }).isEmpty(),
-], usersController.create);
+App.post('/users/new',(usersController.create));
 
 App.listen(__PORT, function () {
   console.log('app listening on port 3000!');
--- Controllers/UsersController.js
+++ Controllers/UsersController.js
@@ -1,63 +1,22 @@
 const User = require('../Models/User'); 
-const { validationResult } = require('express-validator');
+const { check, validationResult } = require('express-validator');
 
-let create = (req,res) => {
-    
+let create = async (req,res) => {
     console.log('llego una petición');
-
+    await check('firstName').isEmpty();
+    await check('lastName').isEmpty();
+    await check('email').isEmail().isEmpty();
+    await check('password').isLength({ min: 6 }).isEmpty();
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-
+ 
     console.log(errors);
 
     return res.send({
         res: 'mori'
     });
-
-    let user = {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        password: req.body.password
-    }
-
-    //Validate data input here
-
-   /*  if(!user.firstName || user.firstName.trim().length === 0){
-        errors.push({
-            field: 'firstName',
-            error: 'This field is required'
-        });
-    }
-
-    if(!user.lasyName || user.lasyName.trim().length === 0){
-        errors.push({
-            field: 'lastName',
-            error: 'This field is required'
-        });
-    }
-
-    if(!user.email || user.email.trim().length === 0){
-        errors.push({
-            field: 'email',
-            error: 'This field is required'
-        });
-    } else {
-        
-    }
-
-    if(!user.password || user.password.trim().length === 0){
-        errors.push({
-            field: 'password',
-            error: 'This field is required'
-        });
-    } */
-
-    user.firstName = user.firstName.trim().toLowerCase();
-    user.lastName = user.lastName.trim().toLowerCase();
-    user.email = user.email.trim().toLowerCase();
 };
 
 module.exports.create = create;
